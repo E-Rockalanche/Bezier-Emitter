0cbff55b35390073f2f24ecaf50dfe666c2b6bca
Eric Roberts <ericjasonroberts@gmail.com>
2018-11-14 14:03:04 -0800

    created lerp functions

---
+++ lerp.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include "lerp.hpp"
+#include "vec3.hpp"
+
+Vec3 lerp(const Vec3& p1, const Vec3& p2, float t) {
+	return p1 + t * (p2 - p1);
+}
+
+Vec3 bezierC1(const Vec3& p1, const Vec3& p2, const Vec3& p3, float t) {
+	const Vec3 v2 = 2 * p2 - 2 * p1;
+	const Vec3 v3 = p3 - 2 * p2 + p1;
+	return p1 + t*v2 + t*t*v3;
+}
+
+Vec3 bezierC2(const Vec3& p1, const Vec3& p2, const Vec3& p3, const Vec3& p4, float t) {
+	const Vec3 v2 = -3 * p1 + 3 * p2;
+	const Vec3 v3 = 3 * p1 - 6 * p2 + 3 * p3;
+	const Vec3 v4 = -p1 + 3 * p2 - 3 * p3 + p4;
+	return p1 + t*v2 + t*t*v3 + t*t*t*v4;
+}
+
+Vec3 catmulRom(const Vec3& p0, const Vec3& p1, const Vec3& p2, const Vec3& p3, float t) {
+	const Vec3 v2 = -1/2 * p0 + 1/2 * p2;
+	const Vec3 v3 = p0 - 5/2 * p1 + 2 * p2 - 1/2 * p3;
+	const Vec3 v4 = -1/2 * p0 + 3/2 * p1 - 3/2 * p2 + 1/2 * p3;
+
+	return p1 + t*v2 + t*t*v3 + t*t*t*v4;
+}
+
+void buildClosedBezierC2(const std::vector<Vec3>& control_points, std::vector<Vec3>& c2_points) {
+	c2_points.clear();
+	const int num_points = control_points.size();
+	for(int i = 0; i < num_points; i++) {
+		const int j = (i + 1) % num_points;
+		const int k = (i + 2) % num_points;
+
+		const Vec3& p0 = control_points[i];
+		const Vec3& p1 = control_points[j];
+		const Vec3& p2 = control_points[k];
+
+		c2_points.push_back((p0 + p1) / 2);
+		c2_points.push_back((p0 + 2 * p1) / 3);
+		c2_points.push_back((2 * p1 + p2) / 3);
+	}
+}
---
+++ lerp.hpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include "vec3.hpp"
+
+Vec3 lerp(const Vec3& p1, const Vec3& p2, float t);
+
+Vec3 bezierC1(const Vec3& p1, const Vec3& p2, const Vec3& p3, float t);
+
+Vec3 bezierC2(const Vec3& p1, const Vec3& p2, const Vec3& p3, const Vec3& p4, float t);
+
+Vec3 catmulRom(const Vec3& p0, const Vec3& p1, const Vec3& p2, const Vec3& p3, float t);
+
+void buildClosedBezierC2(const std::vector<Vec3>& control_points, std::vector<Vec3>& c2_points);
