da41c9bae8837e49298ea1907e57365a55b3bb8f
Eric Roberts <ericjasonroberts@gmail.com>
2018-11-24 16:17:51 -0800

    finished vortices

--- inc/emitter.hpp
+++ inc/emitter.hpp
@@ -22,10 +22,9 @@ public:
 	bool loadFromFile(std::string filename, std::string path = "");
 	void render(Vec3 camera_up = Vec3(0, 1, 0), Vec3 camera_right = Vec3(1, 0, 0));
 	void update(float time);
-	void createParticle();
 
 private:
-	static const int MAX_PARTICLES = 256;
+	static const int DEFAULT_MAX_PARTICLES = 256;
 
 	struct Particle {
 		Vec3 position;
@@ -33,13 +32,30 @@ private:
 
 		float lifetime;
 		float time;
+
+		Particle() {}
+		Particle(Vec3 p, Vec3 v, float lt) : position(p), velocity(v), lifetime(lt), time(0) {}
+		bool isDead() { return time >= lifetime; }
 	};
+
+	struct Vortex : Particle {
+		Vec3 angular_velocity;
+
+		Vortex() {}
+		Vortex(Particle p, Vec3 av) : Particle(p), angular_velocity(av) {}
+	};
+
+	Path::Iterator path_iterator;
 
 	std::vector<Particle> particles;
 	int num_particles;
-	Path::Iterator path_iterator;
 	float emission_rate;
 	float time_since_emission;
+
+	std::vector<Vortex> vortices;
+	int num_vortices;
+	float vortex_emission_rate;
+	float time_since_vortex_emission;
 
 	// environment variables
 
@@ -62,6 +78,12 @@ private:
 	float min_lifetime;
 	float max_lifetime;
 
+	Vec3 min_vortex_axis;
+	Vec3 max_vortex_axis;
+
+	float min_vortex_angular_velocity;
+	float max_vortex_angular_velocity;
+
 	// interpolated variables
 
 	Vec3 colour1;
@@ -73,6 +95,12 @@ private:
 	int texture_handle;
 	int images;
 	int h_images;
+
+	void createParticle();
+	void createVortex();
+	void initializeParticle(Particle& p);
+	void initializeVortex(Vortex& v);
+	void updateParticle(Particle& p, float time);
 };
 
 #endif
